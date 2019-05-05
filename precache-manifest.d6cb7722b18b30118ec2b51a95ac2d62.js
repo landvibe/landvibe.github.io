f735da00b209f2f6bbe8f9f5b389ae7c38612ef1
landvibe.podo <landvibe@kakaopage.com>
2019-05-05 23:53:33 +0900

    initial commit

---
+++ precache-manifest.d6cb7722b18b30118ec2b51a95ac2d62.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9146d75173d81005401ee9890b4b5fa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c65d23a70d87d492418e",
+    "url": "/static/js/2.4542d2a3.chunk.js"
+  },
+  {
+    "revision": "3894d59b146cb92c6d15",
+    "url": "/static/js/main.254659ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
